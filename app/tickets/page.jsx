bf5a17a660e08dcc8d376a0263134905bda9efec
hamza odowa <hamza2.odowa@live.uwe.ac.uk>
2024-01-03 16:49:34 +0000

    made a button to navigate to the createTicket page

--- app/tickets/page.jsx
+++ app/tickets/page.jsx
@@ -1,15 +1,19 @@
+import Link from "next/link";
 import React, { Suspense } from "react";
 import Loading from "../loading";
 import TicketList from "./TicketList";
 
-export default function tickets() {
+export default function Tickets() {
   return (
     <main>
-      <nav>
+      <nav style={{ display: "flex", justifyContent: "space-between" }}>
         <div>
-          <h1>Ticket List</h1>
+          <h1 className="text-2xl">Ticket List</h1>
           <p>Currently open tickets</p>
         </div>
+        <Link href="/tickets/create">
+          <h1 className="text-xl">Create New Ticket</h1>
+        </Link>
       </nav>
       <Suspense fallback={<Loading />}>
         <TicketList />
