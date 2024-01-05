e7f6a1b889badc914294fe1e3b4372a9adb48bdd
hamza odowa <hamza2.odowa@live.uwe.ac.uk>
2024-01-05 11:00:04 +0000

    added comments

--- app/(dashboard)/tickets/page.jsx
+++ app/(dashboard)/tickets/page.jsx
@@ -1,27 +1,61 @@
-import Link from "next/link";
-import React, { Suspense } from "react";
-import Loading from "../loading";
-import TicketList from "./TicketList";
+import { notFound } from "next/navigation";
 
-export const metadata = {
-  title: "New Helpdesk | Tickets ",
-};
+export const dynamicParams = true; // default value = true
 
-export default function Tickets() {
+// Generates metadata for the ticket details page
+export async function generateMetadata({ params }) {
+  const id = params.id; // Extracting ID from params
+
+  const res = await fetch(`http://localhost:4000/tickets/${id}`);
+  const ticket = await res.json();
+
+  return {
+    title: `New | ${ticket.title}`, // Generating title based on ticket data
+  };
+}
+
+// Generates static params using fetched ticket data
+export async function generateStaticParams() {
+  const res = await fetch("http://localhost:4000/tickets");
+  const tickets = await res.json();
+
+  return tickets.map((ticket) => ({
+    id: ticket.id,
+  }));
+}
+
+// Fetches ticket details based on ID
+async function getTicket(id) {
+  const res = await fetch(`http://localhost:4000/tickets/${id}`, {
+    next: {
+      revalidate: 60,
+    },
+  });
+
+  if (!res.ok) {
+    notFound(); // Handle not found cases
+  }
+
+  return res.json();
+}
+
+// Component to display ticket details
+export default async function TicketDetails({ params }) {
+  const ticket = await getTicket(params.id.title); // Fetches ticket based on ID
+
   return (
     <main>
-      <nav style={{ display: "flex", justifyContent: "space-between" }}>
-        <div>
-          <h1 className="text-2xl">Ticket List</h1>
-          <p>Currently open tickets</p>
-        </div>
-        <Link href="/tickets/create">
-          <h1 className="text-xl">Create New Ticket</h1>
-        </Link>
+      <nav>
+        <h2>Ticket Details</h2>
       </nav>
-      <Suspense fallback={<Loading />}>
-        <TicketList />
-      </Suspense>
+      <div className="card">
+        <h3>{ticket.title}</h3>
+        <small>Created by {ticket.user_email}</small>
+        <p>{ticket.body}</p>
+        <div className={`pill ${ticket.priority}`}>
+          {ticket.priority} priority
+        </div>
+      </div>
     </main>
   );
 }
