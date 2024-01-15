b7c4d16212dc30c581d2737d350afa9af2bb88a5
hamza odowa <hamza2.odowa@live.uwe.ac.uk>
2024-01-15 11:12:23 +0000

    redirect users to homepage if logged in to not see the login page

--- app/(auth)/layout.jsx
+++ app/(auth)/layout.jsx
@@ -1,7 +1,20 @@
 import Link from "next/link";
-import React from "react";
+import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
+import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-export default function AuthLayout({ children }) {
+export default async function AuthLayout({ children }) {
+  // Create a Supabase client instance
+  const supabase = createServerComponentClient({ cookies });
+
+  // Retrieve the current session data from Supabase
+  const { data } = await supabase.auth.getSession();
+
+  // Check if there is an active session; otherwise, redirect to the login page
+  if (data.session) {
+    redirect("/");
+  }
+
   return (
     <>
       <nav>
