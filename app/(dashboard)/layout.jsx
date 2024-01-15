db6737e3152ae209f7ee7051019a61cccffb70d4
hamza odowa <hamza2.odowa@live.uwe.ac.uk>
2024-01-15 11:17:22 +0000

    deny access to unregistered users to access app

--- app/(dashboard)/layout.jsx
+++ app/(dashboard)/layout.jsx
@@ -1,29 +1,28 @@
+// Import necessary modules and components
 import { createServerComponentClient } from "@supabase/auth-helpers-nextjs";
 import { cookies } from "next/headers";
+import { redirect } from "next/navigation";
 
-// components
+// Import the Navbar component
 import Navbar from "@/app/components/Navbar";
 
+// DashboardLayout component that wraps the application layout
 export default async function DashboardLayout({ children }) {
-  let user = null;
+  // Create a Supabase client instance
+  const supabase = createServerComponentClient({ cookies });
 
-  try {
-    const supabase = createServerComponentClient({ cookies });
+  // Retrieve the current session data from Supabase
+  const { data } = await supabase.auth.getSession();
 
-    // getting the current session and extracting the data from it
-    const { data } = await supabase.auth.getSession();
-
-    // Checking if data and user information exist before accessing it
-    if (data && data.session && data.session.user) {
-      user = data.session.user;
-    }
-  } catch (error) {
-    console.error("Error fetching user data:", error);
+  // Check if there is an active session; otherwise, redirect to the login page
+  if (!data.session) {
+    redirect("/login");
   }
 
+  // Render the Navbar component with the user information and the child components
   return (
     <>
-      <Navbar user={user} />
+      <Navbar user={data.session.user} />
       {children}
     </>
   );
